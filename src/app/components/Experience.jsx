cbf8477772d860519c8baafd77c3a5b40f469785
Alexander Bris <alexbris21@gmail.com>
2024-04-23 16:43:47 -0300

    nuevos avances agregue los iconos de las tecnologias usadas en cada proyecto y adapte mejor el responsive

--- src/app/components/Experience.jsx
+++ src/app/components/Experience.jsx
@@ -7,10 +7,10 @@ const Experience = () => {
         <h1 className={`${ibmPlexMono.className} text-4xl text-celestito my-5`}>Donde trabaje</h1>
             <h2 className={`${ibmPlexMono.className} text-slate-300 text-2xl`}>Frontend Dev <span className={`${ibmPlexMono.className} text-celestito text-2xl`}>Vopper</span></h2>
             <p className={`${ibmPlexMono.className} text-xs my-3`}>Ene 2024 - Present</p>
-            <p className="my-3 text-base"><span className="text-verdecito">▹</span> Crear componentes reutilizables para garantizar la eficiencia y consistencia del código.</p>
-            <p className="my-3 text-base"><span className="text-verdecito">▹</span> Diseñar la arquitectura de la aplicación para asegurar su escalabilidad y mantenibilidad a largo plazo.</p>
-            <p className="my-3 text-base"><span className="text-verdecito">▹</span> Gestionar el flujo de datos entre los diferentes componentes para garantizar una experiencia fluida.</p>
-            <p className="my-3 text-base"><span className="text-verdecito">▹</span> Trabajar en colaboración con el equipo backend para integrar la lógica de negocio y garantizar una experiencia de usuario adecuada.</p>
+            <p className="my-3 text-base"><span className="text-verdecito">▹</span> Presenté proyectos completos a clientes de manera autónoma, comunicando eficazmente el progreso y los detalles técnicos del desarrollo para asegurar la satisfacción del cliente y la aprobación del proyecto.</p>
+            <p className="my-3 text-base"><span className="text-verdecito">▹</span> Desarrollé y diseñé el frontend completo de aplicaciones utilizando tecnologías como Svelte, TailwindCSS y Typescript, asegurando una experiencia de usuario atractiva y funcional.</p>
+            <p className="my-3 text-base"><span className="text-verdecito">▹</span>Coordiné con otros miembros del equipo para garantizar la entrega a tiempo de los proyectos y mantener una comunicación clara con los clientes durante el ciclo de vida del desarrollo.</p>
+            <p className="my-3 text-base"><span className="text-verdecito">▹</span>Trabaje en colaboración con el equipo backend para integrar la lógica de negocio y garantizar una experiencia de usuario adecuada.</p>
         </div>
     </section>
   )
--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -3,48 +3,72 @@ import { SiTailwindcss } from "react-icons/si";
 import { SiTypescript } from "react-icons/si";
 import { SiNestjs } from "react-icons/si";
 import { SiMongodb } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { SiNodedotjs } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { SiSequelize } from "react-icons/si";
 import Image from "next/image";
 import { ibmPlexMono } from "../ui/fonts";
-import {useMediaQuery} from "react-responsive";
+import { useMediaQuery } from "react-responsive";
 
 const Projects = () => {
-
   const screenMobile = useMediaQuery({ maxWidth: 550 });
 
-  return (  
-    <section id="work" className="lg:h-screen h-full text-slate-300 flex justify-center items-center flex-col gap-5">
-      <h1 className={`${ibmPlexMono.className} text-2xl pb-4`}>Algunos proyectos que desarrolle</h1>
-      <div className={`flex flex-col lg:flex-row gap-5 ex:w-full w-80 md:w-1/2 lg:min-h-44 p-3 rounded-md hover:bg-slate-800/50 hover:shadow-md border-slate-600 hover:border-t ${screenMobile ? 'bg-slate-800/50 shadow-md border-slate-600 border-t' : ''}`}>
-        <div className="w-96 h-20">
+  return (
+    <section
+      id="work"
+      className="lg:h-screen h-full text-slate-300 flex justify-center items-center flex-col gap-5"
+    >
+      <h1 className={`${ibmPlexMono.className} text-2xl pb-4`}>
+        Algunos proyectos que desarrolle
+      </h1>
+      <div
+        className={`flex flex-col lg:flex-row gap-5 ex:w-full w-80 md:w-1/2 lg:min-h-44 p-3 rounded-md hover:bg-slate-800/50 hover:shadow-md border-slate-600 hover:border-t ${
+          screenMobile
+            ? "bg-slate-800/50 shadow-md border-slate-600 border-t"
+            : ""
+        }`}
+      >
+        <div className="w-full h-20">
           <Image
             className="lg:h-full lg:w-full rounded-md"
             width="200"
-            height="150"
+            height="120"
             src="/home-prestamista.webp"
             alt="example 1"
           />
         </div>
         <div>
-          <h2 className={`${ibmPlexMono.className} text-lg pb-2`}>Prestamista</h2>
+          <h2 className={`${ibmPlexMono.className} text-lg pb-1`}>
+            Prestamista
+          </h2>
           <p className="text-sm w-full">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-            inventore voluptas, enim atque odio incidunt at similique recusandae
-            dolor vel eveniet nisi ratione dignissimos nam, sed, officia
-            reiciendis lorem20!
+            Desarrollé el frontend completo de una aplicación web para un
+            cliente, dedicada a la gestión de préstamos financieros. Permitiendo
+            al administrador agregar clientes con información personal, detalles
+            de contacto y datos financieros relevantes.
           </p>
-          <div className="flex my-3 gap-3">
-          <SiSvelte />
-          <SiTailwindcss />
-          <SiTypescript />
-          <SiNestjs />
-          <SiMongodb />
+          <div className="flex my-2 gap-3">
+            <SiSvelte />
+            <SiTailwindcss />
+            <SiTypescript />
+            <SiNodedotjs />
+            <SiExpress />
+            <SiMongodb />
           </div>
           <p></p>
         </div>
       </div>
 
-      <div className={`flex flex-col lg:flex-row gap-5 ex:w-full w-80 md:w-1/2 lg:min-h-44 p-3 rounded-md hover:bg-slate-800/50 hover:shadow-md border-slate-600 hover:border-t ${screenMobile ? 'bg-slate-800/50 shadow-md border-slate-600 border-t' : ''}`}>
-        <div className="w-96 h-20">
+      <div
+        className={`flex flex-col lg:flex-row gap-5 ex:w-full w-80 md:w-1/2 lg:min-h-44 p-3 rounded-md hover:bg-slate-800/50 hover:shadow-md border-slate-600 hover:border-t ${
+          screenMobile
+            ? "bg-slate-800/50 shadow-md border-slate-600 border-t"
+            : ""
+        }`}
+      >
+        <div className="w-full h-20">
           <Image
             className="lg:h-full lg:w-full rounded-md"
             width="200"
@@ -54,26 +78,32 @@ const Projects = () => {
           />
         </div>
         <div>
-          <h2 className={`${ibmPlexMono.className} lg:flex text-lg pb-2`}>Learnflow</h2>
+          <h2 className={`${ibmPlexMono.className} lg:flex text-lg pb-1`}>
+            Learnflow
+          </h2>
           <p className="text-sm w-full">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-            inventore voluptas, enim atque odio incidunt at similique recusandae
-            dolor vel eveniet nisi ratione dignissimos nam, sed, officia
-            reiciendis lorem20!
+          Trabajé en los inicio de una aplicación web con inteligencia artificial que permite a los usuarios aprender de manera dinámica y divertida. Me enfoqué principalmente en el frontend, capturando datos del backend y renderizando en la interfaz de usuario.
+          
           </p>
-          <div className="flex my-3 gap-3">
-          <SiSvelte />
-          <SiTailwindcss />
-          <SiTypescript />
-          <SiNestjs />
-          <SiMongodb />
+          <div className="flex my-2 gap-3">
+            <SiSvelte />
+            <SiTailwindcss />
+            <SiTypescript />
+            <SiNestjs />
+            <SiMongodb />
           </div>
           <p></p>
         </div>
       </div>
 
-      <div className={`flex flex-col lg:flex-row gap-5 ex:w-full w-80 md:w-1/2 lg:min-h-44 p-3 rounded-md hover:bg-slate-800/50 hover:shadow-md border-slate-600 hover:border-t ${screenMobile ? 'bg-slate-800/50 shadow-md border-slate-600 border-t' : ''}`}>
-        <div className="w-96 h-20">
+      <div
+        className={`flex flex-col lg:flex-row gap-5 ex:w-full w-80 md:w-1/2 lg:min-h-44 p-3 rounded-md hover:bg-slate-800/50 hover:shadow-md border-slate-600 hover:border-t ${
+          screenMobile
+            ? "bg-slate-800/50 shadow-md border-slate-600 border-t"
+            : ""
+        }`}
+      >
+        <div className="w-full h-20">
           <Image
             className="lg:h-full lg:w-full rounded-md"
             width="200"
@@ -83,19 +113,18 @@ const Projects = () => {
           />
         </div>
         <div>
-          <h2 className={`${ibmPlexMono.className} lg:flex text-lg pb-2`}>ecoWise</h2>
+          <h2 className={`${ibmPlexMono.className} lg:flex text-lg pb-1`}>
+            ecoWise
+          </h2>
           <p className="text-sm w-full">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
-            inventore voluptas, enim atque odio incidunt at similique recusandae
-            dolor vel eveniet nisi ratione dignissimos nam, sed, officia
-            reiciendis lorem20!
+          Como parte de un bootcamp, colaboré en un proyecto grupal para desarrollar una web de productos sustentables y ecológicos. Implementé funcionalidades como login, búsquedas, filtros, paginación, reseñas, pasarela de pago y un panel de administrador.
           </p>
-          <div className="flex my-3 gap-3">
-          <SiSvelte />
-          <SiTailwindcss />
-          <SiTypescript />
-          <SiNestjs />
-          <SiMongodb />
+          <div className="flex my-2 gap-3">
+            <FaReact />
+            <SiTailwindcss />
+            <SiExpress />
+            <SiSequelize />
+            <BiLogoPostgresql />
           </div>
           <p></p>
         </div>
